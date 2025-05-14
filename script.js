a7decf84eae725ccf5a913435b3e9f6e540bd589
Eduardo Segura Rodriguez <segura2011@live.com>
2025-05-14 00:27:27 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,126 @@
-function onScanSuccess(decodedText, decodedResult)
-{
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-    document.getElementById("reader__result").innerHTML = ` Resultado: <b>${decodedText}</b>`;
+// Variables
+const lacteos_cat = document.getElementById("cat1");
+const selectElements = document.querySelectorAll('select');
+const button_add = document.getElementById("button_add");
+
+const listaProductos = document.getElementById("listaProductos");
+
+
+
+
+
+
+
+
+function pagLacteos(){
+    window.open("lacteos.html", "_self");
 }
-function onScanError(errorMessage) {
-  // console.warn(`Scan error: ${errorMessage}`);
+function pagCarnes(){
+    window.open("carnes.html", "_self");
+}
+function pagInicio(){
+    window.open("index.html", "_self");
+}
+function pagCansta(){
+    window.open("canasta.html", "_self");
 }
 
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader", { fps: 10, qrbox: {width: 300, height: 120}});
-html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+const startNumber = 0;
+const endNumber = 20; 
+
+  // Iterar sobre cada elemento select encontrado
+  selectElements.forEach(selectElement => {
+    selectElement.innerHTML = '';
+
+    // Generar las opciones dinámicamente para el select actual
+    for (let i = startNumber; i <= endNumber; i++) {
+      const option = document.createElement('option');
+      option.value = i;
+      option.textContent = i;
+      selectElement.appendChild(option);
+    }
+  });
+
+  function addProducto(){
+
+    let productos = JSON.parse(sessionStorage.getItem('productosSeleccionados')) || [];
+
+
+    selectElements.forEach(selectElement => {
+
+        const id = selectElement.dataset.productId;
+        const producto = selectElement.name;
+        const cantidad = selectElement.value;
+
+        if (cantidad != 0) {
+            const item = {
+              id: id,
+              nombre: producto,
+              cantidad: cantidad
+            };
+      
+            // Verificar si ya existe un producto con ese id
+            const existente = productos.find(p => p.id === id);
+      
+            if (existente) {
+              // Si ya existe, actualiza la cantidad
+              existente.cantidad = cantidad;
+            } else {
+              // Si no existe, lo agrega
+              productos.push(item);
+            }
+      
+            console.log(`ID: ${id}, Producto: ${producto}, Cantidad: ${cantidad}`);
+          }
+      });
+
+      sessionStorage.setItem('productosSeleccionados', JSON.stringify(productos));
+  }
+
+
+const productosData = JSON.parse(sessionStorage.getItem('productosSeleccionados'));
+
+listaProductos.innerHTML = '';
+if (productosData) {
+    productosData.forEach(producto => {
+        const li = document.createElement('li');
+        li.textContent = `${producto.nombre}............Cantidad: ${producto.cantidad}`;
+        listaProductos.appendChild(li);
+    });
+}
+
+
+
+function borrarData(){
+    sessionStorage.removeItem('productosSeleccionados');
+    listaProductos.innerHTML = '';
+    selectElements.forEach(selectElement => {
+        selectElement.value = 0;
+    });
+}
+
+console.log(sessionStorage.getItem('productosSeleccionados'));
+
+
+function enviarData(){
+  fetch("https://script.google.com/macros/s/AKfycbykko1qMU8C-n7kkHA7Iph4W0aAGdrvSAsS03O9FumxLzSmen5_wpkp6TTVnLo6Hx9o2A/exec", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(productosData)
+  })
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Error en la solicitud');
+  }
+  return response.json();
+})
+.then(data => {
+  console.log('Respuesta del servidor:', data);
+})
+.catch(error => {
+  console.error('Hubo un problema con la petición:', error);
+});
+}
